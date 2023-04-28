1f5df85ec854c59240cbd4c62475ebd573277af2
Sean Link <sean.link@netally.com>
2023-04-28 16:54:45 -0600

    Pushing to test

--- flake.nix
+++ flake.nix
@@ -18,9 +18,9 @@
     };
   };
 
-  outputs = { self, inputs, ... }: {
+  outputs = { self, nixpkgs, home-manager, jovian, nixos-hardware, ... }: {
     nixosConfigurations = {
-      steamdeck = inputs.nixpkgs.lib.nixosSystem {
+      sean-steamdeck = nixpkgs.lib.nixosSystem { # hostname
         system = "x86_64-linux";
         modules = [
           # Import the NixOS and nixos-hardware modules
@@ -29,12 +29,31 @@
           
           # Configure your system settings
           ({ pkgs, lib, ... }: {
+            nixpkgs.config.allowUnfree = true;
+            nix.extraOptions = '' 
+              experimental-features = nix-command flakes
+            '';
+            hardware.bluetooth.enable = true;
+            networking.networkmanager.enable = true;
+            time.timeZone = "America/Denver";
+            services.xserver.xkbOptions = "caps:escape";
+            services.xserver.enable = true;
+            services.xserver.displayManager.sddm.enable = true;
+            services.xserver.desktopManager.plasma5.enable = true;
+            services.printing.enable = true;
+            swapDevices = [ { device = "/swapfile"; size = 1024; } ];
+
+            # Enable touchpad support
+            services.xserver.libinput.enable = true;
+
+            sound.enable = true;
+            hardware.pulseaudio.enable = true;
             imports = [
               # Import the home-manager module
-              inputs.home-manager.nixosModules.home-manager
+              #inputs.home-manager.nixosModules.home-manager
               
               # Import any other modules you need
-              "${inputs.jovian}/modules"
+              "${jovian}/modules"
             ];
 
             jovian = {
