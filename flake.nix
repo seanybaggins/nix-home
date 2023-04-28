163eb248726ab4fa82f74b1290fd28fb60f820ff
Sean Link <sean.link@netally.com>
2023-04-28 17:08:34 -0600

    Pushing to test

--- flake.nix
+++ flake.nix
@@ -2,16 +2,12 @@
   description = "System configurations for all of my personal machines";
 
   inputs = {
-
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     jovian = {
       url = "github:Jovian-Experiments/Jovian-NixOS";
       flake = false;
@@ -20,17 +16,12 @@
 
   outputs = { self, nixpkgs, home-manager, jovian, nixos-hardware, ... }: {
     nixosConfigurations = {
-      sean-steamdeck = nixpkgs.lib.nixosSystem { # hostname
+      sean-steamdeck = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          # Import the NixOS and nixos-hardware modules
-          #nixpkgs.nixosModules
-          #nixos-hardware.nixosModules
-          
-          # Configure your system settings
           ({ pkgs, lib, ... }: {
             nixpkgs.config.allowUnfree = true;
-            nix.extraOptions = '' 
+            nix.extraOptions = ''
               experimental-features = nix-command flakes
             '';
             hardware.bluetooth.enable = true;
@@ -48,11 +39,8 @@
 
             sound.enable = true;
             hardware.pulseaudio.enable = true;
+
             imports = [
-              # Import the home-manager module
-              #inputs.home-manager.nixosModules.home-manager
-              
-              # Import any other modules you need
               "${jovian}/modules"
             ];
 
@@ -61,27 +49,14 @@
               devices.steamdeck.enable = true;
             };
 
-            # System settings and packages
             system.stateVersion = "unstable";
             boot.loader.systemd-boot.enable = true;
             boot.loader.efi.canTouchEfiVariables = true;
-
-            # Set your desired hostname
             networking.hostName = "sean-steamdeck";
-            
-            # Enable home-manager
-            #programs.home-manager.enable = true;
-            
-            # Define a user and enable home-manager for that user
-            #users.users.myuser = {
-            #  isNormalUser = true;
-            #  home.stateVersion = "unstable";
-            #  extraGroups = [ "wheel" "networkmanager" ];
-            #  shell = pkgs.zsh;
-            #};
           })
         ];
       };
     };
   };
 }
+
