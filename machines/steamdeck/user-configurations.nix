d4d8fd9c18265652d7bc3dc0a6af2674a2141fbf
Sean Link <seanybaggins1@gmail.com>
2023-06-13 14:56:04 -0600

    Fixed a bug where my system would not boot is the SD card is not propery read at boot.

--- machines/steamdeck/user-configurations.nix
+++ machines/steamdeck/user-configurations.nix
@@ -3,6 +3,7 @@
   fileSystems."/home/sean/lightdeck/yocto" = {
     device = "/dev/disk/by-uuid/5c7f8cf8-5613-4ad7-a4b2-62cd963352e2"; 
     fsType = "ext4"; 
+    options = [ "nofail" ]; # boot even when the SD card is not present
   };
 
   # Make sure the yocto user is owned by sean and not root.
