d04cbe4adfdcc5057d028601b380b0d12a955297
Sean Link <seanybaggins1@gmail.com>
2023-06-13 14:46:00 -0600

    Now have my SSD card auto mounting to the yocto dir.

--- machines/steamdeck/user-configurations.nix
+++ machines/steamdeck/user-configurations.nix
@@ -1,4 +1,12 @@
 { ... }:
 {
+  fileSystems."/home/sean/lightdeck/yocto" = {
+    device = "/dev/disk/by-uuid/5c7f8cf8-5613-4ad7-a4b2-62cd963352e2"; 
+    fsType = "ext4"; 
+  };
 
+  # Make sure the yocto user is owned by sean and not root.
+  systemd.tmpfiles.rules = [
+    "d /home/sean/lightdeck/yocto 0755 sean users - -"
+  ];
 }
